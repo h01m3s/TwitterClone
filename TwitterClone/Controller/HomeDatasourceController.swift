2de397729cbb00d3c988e53812d4c5e1d1deb2b1
h01m3s <lin426797@gmail.com>
2018-02-02 01:07:16 -0500

    Caching and Loading Images using CachedImageView

--- TwitterClone/Controller/HomeDatasourceController.swift
+++ TwitterClone/Controller/HomeDatasourceController.swift
@@ -58,18 +58,30 @@ class HomeDatasourceController: DatasourceController {
     
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        if let user = self.datasource?.item(indexPath) as? User {
+        // suggest users section
+        if indexPath.section == 0 {
+            guard let user = self.datasource?.item(indexPath) as? User else { return .zero}
+            let estimatedHeight = estimatedHeightForText(user.bioText)
             
-            let approximatedWidthOfBioTextView = view.frame.width - 12 - 50 - 12 - 2
-            let size = CGSize(width: approximatedWidthOfBioTextView, height: 1000)
-            let attributes = [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 15)]
-            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+            return CGSize(width: view.frame.width, height: estimatedHeight + 66)
+        } else if indexPath.section == 1 {
+            // tweet section
             
-            return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+            guard let tweet = datasource?.item(indexPath) as? Tweet else { return .zero}
+            let estimatedHeight = estimatedHeightForText(tweet.message)
+            
+            return CGSize(width: view.frame.width, height: estimatedHeight + 74)
         }
         
-        
         return CGSize(width: view.frame.width, height: 200)
+    }
+    
+    private func estimatedHeightForText(_ text: String) -> CGFloat {
+        let approximatedWidthOfBioTextView = view.frame.width - 12 - 50 - 12 - 2
+        let size = CGSize(width: approximatedWidthOfBioTextView, height: 1000)
+        let attributes = [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 15)]
+        let estimatedFrame = NSString(string: text).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+        return estimatedFrame.height
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
--- TwitterClone/View/TweetCell.swift
+++ TwitterClone/View/TweetCell.swift
@@ -14,6 +14,8 @@ class TweetCell: DatasourceCell {
         didSet {
             guard let tweet = datasourceItem as? Tweet else { return }
             
+            profileImageView.loadImage(urlString: tweet.user.profileImageUrl)
+            
             let attributedText = NSMutableAttributedString(string: tweet.user.name, attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 16)])
             
             let usernameString = "  \(tweet.user.username)\n"
@@ -38,8 +40,8 @@ class TweetCell: DatasourceCell {
         return tv
     }()
     
-    let profileImageView: UIImageView = {
-        let imageView = UIImageView()
+    let profileImageView: CachedImageView = {
+        let imageView = CachedImageView()
         imageView.image = #imageLiteral(resourceName: "deadpool_image")
         imageView.layer.cornerRadius = 5
         imageView.clipsToBounds = true
